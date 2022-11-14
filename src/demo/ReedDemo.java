40deea016de89d04e0748a0795c70feef4fa9b30
RuidiH <764342051>
2022-11-14 07:42:17 -0800

    added basic information, treasure collections, and frame icon to demo/ReedDemo.java

--- src/demo/ReedDemo.java
+++ src/demo/ReedDemo.java
@@ -8,12 +8,18 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
+import java.util.Random;
+import javax.print.DocFlavor.URL;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 
@@ -25,17 +31,29 @@ import javax.swing.border.LineBorder;
  */
 
 public class ReedDemo {
-  // define rows and columns
+  // define dungeon map constants.
   private final int ROW = 8;
   private final int COL = 8;
+  private final int seed = 0;
+  private final int interconnectivity = 0;
+  private int treasurePercentage = 20;
+  private final Random random = new Random(seed);
+  
+  // other varibles:
+  private int treasureCount = 0;
   
   // define Panel and buttons
   private JPanel gui = new JPanel(new BorderLayout(3, 3));
-  private JButton[][] dungeonSpace = new JButton[ROW][COL];
+  private JButton[][] dungeonButtons = new JButton[ROW][COL];
   private JPanel dungeon;
-  private DungeonSpace[][] dungeonArray;
+  private JPanel infoDisplay = new JPanel();
   private Position current = new Position(0, 0);
+  private DungeonSpace[][] dungeonArray;
   private boolean wrap = true;
+  private String playerName = "Wolffurry\n";
+  
+  // player
+  private Player player;
   
   /**
    * default public constructor.
@@ -49,8 +67,36 @@ public class ReedDemo {
    *  generate a random map using MapGenerator.
    */
   private void initializeDungeon() {
-    MapGenerator map = new MapGenerator(this.ROW, this.COL, wrap, 0, 0);
+    MapGenerator map = new MapGenerator(this.ROW, 
+        this.COL, this.wrap, this.interconnectivity, this.seed);
     this.dungeonArray = map.generateMazeSpace();
+    this.generateTreasure();
+    this.player = new DungeonPlayer(this.playerName, this.current);
+  }
+  
+  /**
+   * add treasures to the dungeon.
+   */
+  private void generateTreasure() {
+    int caveCounter = 0;
+    for (DungeonSpace[] d : this.dungeonArray) {
+      caveCounter += Arrays.stream(d).filter(i -> i instanceof Cave).count();
+    }
+    int caveHasTreasure = (int) Math.ceil(caveCounter * this.treasurePercentage / 100.0);
+    this.treasureCount = caveHasTreasure;
+    while (caveHasTreasure > 0) {
+      int row = this.random.nextInt(this.ROW);
+      int col = this.random.nextInt(this.COL);
+      if (this.dungeonArray[row][col] instanceof Cave) {
+        Cave c = (Cave) this.dungeonArray[row][col];
+        if (!c.hasTreasure()) {
+          c.addTreasure();
+          caveHasTreasure -= 1;
+        } else {
+          c.addTreasure();
+        }
+      }
+    }
   }
   
   /**
@@ -65,6 +111,7 @@ public class ReedDemo {
    */
   public void updateGui() {
     this.gui.removeAll();
+    this.infoDisplay.removeAll();
     this.drawMaze();
     this.gui.revalidate();
     this.gui.repaint();
@@ -81,8 +128,8 @@ public class ReedDemo {
     
     // draw space
     Insets spaceMargin = new Insets(0, 0, 0, 0);
-    for (int i = 0; i < this.dungeonSpace.length; i++) {
-      for (int j = 0; j < this.dungeonSpace[i].length; j++) {
+    for (int i = 0; i < this.dungeonButtons.length; i++) {
+      for (int j = 0; j < this.dungeonButtons[i].length; j++) {
         // create buttons
         JButton b = new JButton();
         b.setMargin(spaceMargin);
@@ -105,17 +152,17 @@ public class ReedDemo {
             JButton b = (JButton)e.getSource();
             if (b.getBackground().equals(Color.green)) {
               current = (Position) b.getClientProperty(current);
+              player.moveTo(current);
               updateGui();
             }
           }
         });
         
         // add private 2D array variable
-        this.dungeonSpace[i][j] = b;
+        this.dungeonButtons[i][j] = b;
       }
     }
-    // change color as needed:
-//    this.dungeonSpace[this.current.getRow()][this.current.getCol()].setBackground(Color.blue);
+    // color every valid adjacent dungeon space.
     DungeonSpace current = this.dungeonArray[this.current.getRow()][this.current.getCol()];
     if (current
         .getPathDirection()
@@ -124,7 +171,7 @@ public class ReedDemo {
       if (newRow == -1) {
         newRow = this.ROW - 1;
       }
-      this.dungeonSpace[newRow][this.current.getCol()].setBackground(Color.green);
+      this.dungeonButtons[newRow][this.current.getCol()].setBackground(Color.green);
     }
     if (current
         .getPathDirection()
@@ -133,7 +180,7 @@ public class ReedDemo {
       if (newCol == this.COL) {
         newCol = 0;
       }
-      this.dungeonSpace[this.current.getRow()][newCol].setBackground(Color.green);
+      this.dungeonButtons[this.current.getRow()][newCol].setBackground(Color.green);
     }
     if (current
         .getPathDirection()
@@ -142,7 +189,7 @@ public class ReedDemo {
       if (newRow == this.ROW) {
         newRow = 0;
       }
-      this.dungeonSpace[newRow][this.current.getCol()].setBackground(Color.green);
+      this.dungeonButtons[newRow][this.current.getCol()].setBackground(Color.green);
     }
     if (current
         .getPathDirection()
@@ -151,14 +198,81 @@ public class ReedDemo {
       if (newCol == -1) {
         newCol = this.COL - 1;
       }
-      this.dungeonSpace[this.current.getRow()][newCol].setBackground(Color.green);
+      this.dungeonButtons[this.current.getRow()][newCol].setBackground(Color.green);
     }
-    // add space
+    // add buttons to dungeon panel.
     for (int i = 0; i < this.ROW; i++) {
       for (int j = 0; j < this.COL; j++) {
-        dungeon.add(dungeonSpace[i][j]);
+        dungeon.add(dungeonButtons[i][j]);
       }
     }
+    
+    //add treasure at current location to player.
+    DungeonSpace currentSpace = 
+        this.dungeonArray[this.current.getRow()][this.current.getCol()];
+    if (currentSpace.hasTreasure()) {
+      for (Treasure t : currentSpace.getTreasure()) {
+        this.player.pickUpTreasure(t);
+      }
+    }
+    
+    // adding other components to the gui
+    // intialize boxlayout for infoDisplay panel.
+    BoxLayout box = new BoxLayout(this.infoDisplay, BoxLayout.Y_AXIS);
+    this.infoDisplay.setLayout(box);
+    
+    // add basic player information
+    this.infoDisplay.add(new JLabel(String.format("Player: %s\n", this.playerName)));
+    this.infoDisplay.add(new JLabel("\n"));
+    this.infoDisplay.add(new JLabel(
+        String.format("Position: %s\n", this.current.toString())));
+    this.infoDisplay.add(new JLabel("Treasures: \n"));
+    this.infoDisplay.add(new JLabel("\n"));
+    // Diamond count
+    int diamondCount = 0;
+    if (this.player.getTreasure().get(Treasure.DIAMOND) != null) {
+      diamondCount = this.player.getTreasure().get(Treasure.DIAMOND);
+    }
+    this.infoDisplay.add(new JLabel(
+        String.format("Diamond --- %d", diamondCount)));
+    // Sapphire count
+    int sapphireCount = 0;
+    if (this.player.getTreasure().get(Treasure.SAPPHIRE) != null) {
+      sapphireCount = this.player.getTreasure().get(Treasure.SAPPHIRE);
+    }
+    this.infoDisplay.add(new JLabel(
+        String.format("Sapphire --- %d", sapphireCount)));
+    // Ruby count
+    int rubyCount = 0;
+    if (this.player.getTreasure().get(Treasure.RUBY) != null) {
+      rubyCount = this.player.getTreasure().get(Treasure.RUBY);
+    }
+    this.infoDisplay.add(new JLabel(
+        String.format("Ruby    ---    %d", rubyCount)));
+    
+    JTextPane message = new JTextPane();
+    if (currentSpace.hasTreasure()) {
+      String treasureInfo = "Collected";
+      for (Treasure t : currentSpace.getTreasure()) {
+        treasureInfo = String.format("%s %s", treasureInfo, t.toString());
+      }
+      this.dungeonArray[this.current.getRow()][this.current.getCol()].removeTreasures();
+      message.setText(treasureInfo);
+    }
+    this.infoDisplay.add(message);
+    
+//    this.infoDisplay.add(new JLabel("Treasure at current location:\n"));
+//    String treasureInfo = "";
+//    DungeonSpace currentSpace = this.dungeonArray[this.current.getRow()][this.current.getCol()];
+//    if (currentSpace.getTreasure() != null && !currentSpace.getTreasure().isEmpty()) {
+//      for (Treasure t : currentSpace.getTreasure()) {
+//        treasureInfo = String.format("%s%s\n", treasureInfo, t.toString());
+//      }
+//    } else {
+//      treasureInfo = String.format("%sNone\n", treasureInfo);
+//    }
+//    this.infoDisplay.add(new JLabel(treasureInfo));
+    this.gui.add(infoDisplay, BorderLayout.LINE_START);
   }
   
   public final JComponent getGui() {
@@ -167,9 +281,14 @@ public class ReedDemo {
   
   public static void main(String[] args) {
     ReedDemo demo = new ReedDemo();
-    JFrame f = new JFrame("Test");
+    JFrame f = new JFrame("Project 4 Demo");
+    // set frame icon
+    java.net.URL imageURL = ReedDemo.class.getResource("/arts/hoyoverseIcon.jpeg");
+    ImageIcon frameIcon = new ImageIcon(imageURL);
+    f.setIconImage(frameIcon.getImage());
+    
     f.add(demo.getGui());
-    f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     f.setLocationByPlatform(true);
     f.pack();
     
